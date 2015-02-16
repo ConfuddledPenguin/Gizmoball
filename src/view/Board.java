86d8579544b55080a83960208b93de58e2a76f15
Andrew Scott <andrewScottt@live.com>
2015-02-16 19:51:59 +0000

    Revert "Update GUI to add shapes"
    This reverts commit b089f6217afe4f88255c296935f9af3bc7711396.

--- src/view/Board.java
+++ src/view/Board.java
@@ -5,31 +5,27 @@ import interfaces.IView;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-import model.Model;s
+import model.Model;
 
 public  class Board extends JPanel implements Observer, IView {
 
 	private static final long serialVersionUID = 1L;
 	protected int width;
 	protected int height;
-	protected List<Shape> gizmoList;
-	
+
 	public Board(int w, int h, Model m) {
 		
 		width = w;
 		height = h;
-		gizmoList = new ArrayList<Shape>();
+		
 		m.addObserver(this);
+		
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.setPreferredSize(new Dimension(width, height));
 	}
@@ -40,23 +36,34 @@ public  class Board extends JPanel implements Observer, IView {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(new Color(0,0,255));
-		if(!gizmoList.isEmpty()){
-			for (int i = 0; i < gizmoList.size(); i++){
-				g2.draw(gizmoList.get(i));
-				g2.fill(gizmoList.get(i));
-			}
 	}
-}
+	
+	private void addGizmo(){
+		
+	}
+	
+	private void addBall(){
+		
+	}
+	
+	private void addFlipper(){
+		
+	}
+	
+	private void addAbsorber(){
+		
+	}
 	
 	@Override
 	public void update(Observable o, Object arg) {
-	
-		if (arg instanceof Shape)
-			gizmoList.add((Shape)arg);
 		
-		repaint();
+		/*if(arg instanceof Gizmo)
+			addGizmo(arg);
+		else if(arg instanceof Ball)
+			addBall(arg);
+		else if(arg instanceof Flipper)
+			addFlipper(arg);
+		else if(arg instanceof Absorber)
+			addAbsorber(arg);*/
 	}
 }
