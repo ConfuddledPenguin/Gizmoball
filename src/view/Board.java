b089f6217afe4f88255c296935f9af3bc7711396
Andrew Scott <andrewScottt@live.com>
2015-02-16 19:16:07 +0000

    Update GUI to add shapes
    The board will use the update method to add a shape, for this to work
    the model will have to return a shape object through the
    notifyObservers() method.

--- src/view/Board.java
+++ src/view/Board.java
@@ -5,27 +5,31 @@ import interfaces.IView;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-import model.Model;
+import model.Model;s
 
 public  class Board extends JPanel implements Observer, IView {
 
 	private static final long serialVersionUID = 1L;
 	protected int width;
 	protected int height;
-
+	protected List<Shape> gizmoList;
+	
 	public Board(int w, int h, Model m) {
 		
 		width = w;
 		height = h;
-		
+		gizmoList = new ArrayList<Shape>();
 		m.addObserver(this);
-		
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 		this.setPreferredSize(new Dimension(width, height));
 	}
@@ -36,34 +40,23 @@ public  class Board extends JPanel implements Observer, IView {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-	}
-	
-	private void addGizmo(){
 		
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(new Color(0,0,255));
+		if(!gizmoList.isEmpty()){
+			for (int i = 0; i < gizmoList.size(); i++){
+				g2.draw(gizmoList.get(i));
+				g2.fill(gizmoList.get(i));
+			}
 	}
-	
-	private void addBall(){
-		
-	}
-	
-	private void addFlipper(){
-		
-	}
-	
-	private void addAbsorber(){
-		
-	}
+}
 	
 	@Override
 	public void update(Observable o, Object arg) {
+	
+		if (arg instanceof Shape)
+			gizmoList.add((Shape)arg);
 		
-		/*if(arg instanceof Gizmo)
-			addGizmo(arg);
-		else if(arg instanceof Ball)
-			addBall(arg);
-		else if(arg instanceof Flipper)
-			addFlipper(arg);
-		else if(arg instanceof Absorber)
-			addAbsorber(arg);*/
+		repaint();
 	}
 }
