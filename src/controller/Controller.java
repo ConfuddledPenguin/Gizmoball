55a223999dc72695f773e0b5d6d7147e8ffeab56
cameroncclark <xeb12181@uni.strath.ac.uk>
2015-02-17 16:04:27 +0000

    Merge Conflict Resolved

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,13 +3,6 @@ package controller;
 import interfaces.IController;
 import java.awt.event.ActionListener;
 import model.Model;
-<<<<<<< HEAD
-=======
-import model.gizmos.Circle;
-import model.gizmos.Square;
->>>>>>> origin/master
-
-
 
 public class Controller implements IController  {
 	
@@ -23,37 +16,12 @@ public class Controller implements IController  {
 		buildListener = new BuildActionlistner(m);
 	}
 	
-<<<<<<< HEAD
 	public ActionListener getRunListener() {
 		return runListener;
 	}
 
 	public ActionListener getBuildListener() {
 		return buildListener;
-=======
-	public void actionPerformed(ActionEvent e) {
-		
-		System.out.println("Controller: The " + e.getActionCommand()
-				+ " button is clicked at " + new java.util.Date(e.getWhen())
-				+ " with e.paramString " + e.paramString());
-		switch(e.getActionCommand()){
-		
-		case("Square"):
-			model.addGizmo(new Square(0,0));
-			break;
-		
-		case("Left Triangle"):
-			break;
-		
-		case("Circle"):
-			model.addGizmo(new Circle(0,0));
-			break;
-		
-		case("Right Triangle"):
-			break;
-		
-		}
->>>>>>> origin/master
 	}
 
 }
