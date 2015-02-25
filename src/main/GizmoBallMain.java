4b9bceba6c82cd4f274270239608436d88f1f956
cameroncclark <xeb12181@uni.strath.ac.uk>
2015-02-25 19:57:24 +0000

    Quick fix for Absorber
    Can now add in an absorber

--- src/main/GizmoBallMain.java
+++ src/main/GizmoBallMain.java
@@ -1,6 +1,7 @@
 package main;
 
 import model.*;
+import model.gizmos.Absorber;
 import model.gizmos.Circle;
 import model.gizmos.Square;
 import view.GUI;
@@ -18,14 +19,24 @@ public class GizmoBallMain {
 		
 		gui = new GUI('r',m);
 		m.addBall();
-		m.addGizmo(new Circle(10,5));
+		m.addGizmo(new Absorber(0, 20, 30, 1));
+		/*
 		m.addGizmo(new Circle(1,29));
 		m.addGizmo(new Circle(27,29));
 		m.addGizmo(new Circle(10,29));
+		
+		FOR LATER:
+		need to think about how we add the gizmos...
+		if these add gizmos were done before the add 
+		absorber then the absorber wouldnt get added.
+		 
+		*/
 		m.addGizmo(new Circle(15,15));
 		m.addGizmo(new Circle(25,10));
 		m.addGizmo(new Circle(18,9));
 		m.addGizmo(new Circle(14,14));
-		m.addGizmo(new Circle(29,4));
+		m.addGizmo(new Circle(29,4));		
+		m.addGizmo(new Circle(10,5));
+		
 	}
 }
