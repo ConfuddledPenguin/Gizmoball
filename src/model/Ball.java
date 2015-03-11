59849ae4b0fd661ab2cb206b5ffcfab6db846757
Nathan Todd <nathan@gntodd.com>
2015-03-11 16:55:14 +0000

    Ball equals method #31

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -13,20 +13,24 @@ public class Ball implements IBall {
 	private double radius;
 	private double xpos;
 	private double ypos;
-	
+
 	private Vect startVelocity;
 	private double startxpos;
 	private double startypos;
-	
+
 	private boolean stopped = false;
 
 	/**
 	 * The ball constructor
 	 * 
-	 * @param x The x coord
-	 * @param y The y coord
-	 * @param xv The x velocity
-	 * @param yv The y velocity
+	 * @param x
+	 *            The x coord
+	 * @param y
+	 *            The y coord
+	 * @param xv
+	 *            The x velocity
+	 * @param yv
+	 *            The y velocity
 	 */
 	public Ball(double x, double y, double xv, double yv) {
 
@@ -35,13 +39,15 @@ public class Ball implements IBall {
 		xpos = x; // Centre coordinates
 		ypos = y;
 		velocity = new Vect(xv, yv);
-		
+
 		startVelocity = velocity;
 		startxpos = x;
 		startypos = y;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#getVelo()
 	 */
 	@Override
@@ -49,7 +55,9 @@ public class Ball implements IBall {
 		return velocity;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#setVelo(model.physics.Vect)
 	 */
 	@Override
@@ -57,7 +65,9 @@ public class Ball implements IBall {
 		velocity = v;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#getRadius()
 	 */
 	@Override
@@ -65,7 +75,9 @@ public class Ball implements IBall {
 		return radius;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#getCircle()
 	 */
 	@Override
@@ -75,14 +87,18 @@ public class Ball implements IBall {
 	}
 
 	// Ball specific methods that deal with double precision.
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#getExactX()
 	 */
 	public double getExactX() {
 		return xpos * Global.L + radius;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#getExactY()
 	 */
 	public double getExactY() {
@@ -100,7 +116,7 @@ public class Ball implements IBall {
 	public void setY(double newY) {
 		ypos = newY;
 	}
-	
+
 	/* Set x using L as the measurement */
 	public double getX() {
 		return xpos;
@@ -110,29 +126,46 @@ public class Ball implements IBall {
 	public double getY() {
 		return ypos;
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		stopped = true;
 	}
-	
-	public void start(){
+
+	public void start() {
 		stopped = false;
 	}
-	
-	public boolean isStopped(){
+
+	public boolean isStopped() {
 		return stopped;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see model.IBall#reset()
 	 */
-	public void reset(){
-		
+	public void reset() {
+
 		xpos = startxpos;
 		ypos = startypos;
 		velocity = startVelocity;
-		
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		boolean result = false;
+		if (obj instanceof Ball) {
+			Ball ball = (Ball) obj;
+			result = this.xpos == ball.xpos && this.ypos == ball.ypos
+					&& this.velocity.equals(ball.velocity);
+		}
+		return result;
 	}
 
 }
