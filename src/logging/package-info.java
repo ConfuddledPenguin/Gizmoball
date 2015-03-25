52dfa713c17226137dfc9d0ac48a6f2d4d020540
Thomas Maxwell <hello@tom-maxwell.com>
2015-03-25 13:30:24 +0000

    Added package-info files

---
+++ src/logging/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ * The logging package for the Gizmoball application
+ * 
+ * The logging package handles the set up and running of the log
+ */
+package logging;
---
+++ src/main/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ * The main package for the Gizmoball application
+ * 
+ * The main package is responsible for launching the 
+ * application and set up
+ * 
+ */
+package main;
